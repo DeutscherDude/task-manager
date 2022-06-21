07cb81f7fdd22e27410d88098d77d1b92b7aaa1d
DeutscherDude <kolokius@gmail.com>
2022-06-21 18:57:45 +0200

    Proper mocks using 'Partial' and first passing test has been introduced

--- src/middleware/__test__/requestVerifier.test.ts
+++ src/middleware/__test__/requestVerifier.test.ts
@@ -2,13 +2,24 @@ import { verifyBody } from '../requestVerifier';
 import { Request, Response, NextFunction } from 'express';
 
 describe('requestVerifier tests', () => {
-    let mockReq: Request;
-    let mockRes: Response;
-    let next: NextFunction;
-    
+    let mockReq: Partial<Request>;
+    let mockRes: Partial<Response>;
+    let next: NextFunction = jest.fn();
+
     beforeEach(async () => {
         mockReq = {
-            
+
+        }
+        mockRes = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn().mockReturnThis()
         }
     })
+
+    it('should return status code 400 if the body does not meet requirements', async () => {
+        verifyBody(mockReq as Request, mockRes as Response, next);
+        expect(mockRes.status).toBeCalledWith(400);
+        expect(next).not.toBeCalled();
+    })
+
 })
